b4a2843d20c8163d31f21b4b1ae8aff8517a4bdd
mhager3 <mhager3@hotmail.com>
2018-04-08 20:31:25 -0400

    checking in unfinished hw

---
+++ config.py
@@ -0,0 +1,11 @@
+apikey = "a7dc7104c0424607941b32d5acd4deb6"
+
+# openweather api
+api_key ="9b00c988132fc36a9b9d0b176c9d2e31"
+
+# google api key
+gkey = "AIzaSyDtNhNavwXI-0917qnMhj-sKWBxg_7Gbmk"
+
+gkey2 = "AIzaSyAbOW6vBtoLOz844Gm3yLrvT_VshGvKz4s"
+
+gkey3 = "AIzaSyC0tXO0aqwSSgWg-wVnUnUWHNxmeibsWGk"
