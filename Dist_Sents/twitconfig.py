4ca6f9dabb0a5161099b9408228ea6b9961093ec
mhager3 <mhager3@hotmail.com>
2018-04-11 18:56:41 -0400

    uploading tweepy streaming work

---
+++ Dist_Sents/twitconfig.py
@@ -0,0 +1,5 @@
+# twitter api key
+consumer_key = "9GhZkwGZapfXosQ48pnrSaVib"
+consumer_secret = "WgpuHCntdkYARwUWUfx3FbTaDtUP7p5UBomJzziZ1oDByfqbBx"
+access_token = "89519459-4VW7GhWxrGPw8TqDMelXvg2uBnmJKhLTWUnVO6vPw"
+access_token_secret = "36Yj7Honotmojr1dKVamtyIhICawdvRrgdPX1f5SHzoAB"
